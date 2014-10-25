c37cb3bf09b4cd7a8877d61422fa48e76fad090e
wpmcdonald2000 <wpmcdonald2000@gmail.com>
2014-10-25 13:58:34 -0400

    shiny apps folder upload

--- ProjectFiles/ui.R
+++ ProjectFiles/ui.R
@@ -8,11 +8,16 @@ shinyUI(fluidPage(
                 sidebarPanel(
                         helpText("Choose variables "),
                         
-                        sliderInput("integer", "Jitter:", min=0, max=20, value=10),
+                        sliderInput("integer", "Jitter:", min=2, max=20, value=10),
                         
-                        sliderInput("decimal", "Run:", min = 0, max = .010, value = .005, step = .001 ),
+                        sliderInput("decimal", "Run:", min = 0.0001, max = .010, value = .005, step = .001 ),
                         
-                        sliderInput('pi', 'PI factor:', min = 1, max = 10, value = 5, step = 1 )
+                        sliderInput('pi', 'PI factor:', min = 1, max = 10, value = 5, step = 1 ),
+
+                        
+                        
+                        helpText('A playful app to randomly create Spirograph images using the 
+                                 sliders as multipliers for some of the chart output variables')
                         ),
 #       Show the resulting plot using the variable multipliers 
                 mainPanel(
